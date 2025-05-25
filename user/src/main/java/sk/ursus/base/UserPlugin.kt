b749c69914977c55c240eea961064319b551b480
Vlastimil Brecka <vlastimil.brecka@o2.sk>
2025-05-25 03:32:43 +0200

    make provider work

--- user/src/main/java/sk/ursus/base/UserPlugin.kt
+++ user/src/main/java/sk/ursus/base/UserPlugin.kt
@@ -1,17 +1,38 @@
 package sk.ursus.base
 
-import dev.zacsweers.metro.ContributesIntoSet
+import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.IntoSet
+import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
 
 @Inject
 @SingleIn(UserScope::class)
-@ContributesIntoSet(UserScope::class)
-@QualifierFor(UserScope::class)
+@AutoInitializedPlugin
 class UserPlugin(
     private val logger: LOG
 ) : Plugin {
     override fun initialize() {
         logger.d(message = "Hello I'm UserPlugin")
+    }
+}
+
+//@Inject
+//@SingleIn(UserScope::class)
+//@QualifierFor(UserScope::class)
+//@ContributesIntoSet(UserScope::class)
+//class UserPluginWrapperToBeGenerated(
+//    private val userPlugin: UserPlugin
+//) : Plugin by userPlugin
+
+annotation class AutoInitializedPlugin // Hook codegen here
+
+@ContributesTo(UserScope::class)
+interface UserPluginProvider {
+    @Provides
+    @QualifierFor(UserScope::class)
+    @IntoSet
+    fun provideUserPlugin(userPlugin: UserPlugin): Plugin {
+        return userPlugin
     }
 }
